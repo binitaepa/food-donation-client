4708f27e61aa95d67b3d38b13e83d5c234a61094
binitaepa <binitaepa@gmail.com>
2023-11-06 05:07:48 +0600

    review

--- src/Layout/Pages/Review/Review.jsx
+++ src/Layout/Pages/Review/Review.jsx
@@ -1,9 +1,36 @@
 
-
+import {IoIosContacts,IoIosHappy} from 'react-icons/io'
+import {ImShield,ImUserTie} from "react-icons/im";
 const Review = () => {
     return (
         <div>
-            
+               <div className="mt-10 items-center text-center mb-10">
+            <div>
+                <h2 className="text-3xl font-bold text-pink-400 ">Why Choose US?</h2>
+                <hr />
+            </div>
+            <div className="flex flex-col text-red-400 mt-5 font-semibold border text-xl">
+                
+                <div className='flex items-center justify-center'>
+                <h2>Exceptional Quality</h2>
+                <IoIosHappy></IoIosHappy>
+                </div>
+                <div className='flex items-center justify-center'>
+                <h2>Outstanding Customer Service</h2>
+                <IoIosContacts></IoIosContacts> 
+                </div>
+                <div  className='flex items-center justify-center'>
+                <h2>Innovation and Uniqueness</h2>
+                <ImShield></ImShield>
+                </div>
+               <div className='flex items-center justify-center'>
+               <h2>Social Responsibility and Ethics</h2>
+               <ImUserTie></ImUserTie></div> 
+               
+                <hr />
+            </div>
+            <hr />
+        </div>
         </div>
     );
 };
