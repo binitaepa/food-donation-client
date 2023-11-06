1bca3c4a1dee6ff467c3619124b21a31adcf85d9
binitaepa <binitaepa@gmail.com>
2023-11-06 12:11:19 +0600

    card design detail

--- src/Layout/Pages/Feature/FeatureCard.jsx
+++ src/Layout/Pages/Feature/FeatureCard.jsx
@@ -1,25 +1,31 @@
 import { ArrowRightIcon } from "@chakra-ui/icons";
 import { Link } from "react-router-dom";
+import pic from '../../../assets/common-pic.jpg'
 
 
 const FeatureCard = ({feature}) => {
     const {_id,foodName,foodImage,donatorName,foodQuantity,pickupLocation,expiredDateTime,additionalNotes}=feature;
     return (
         <div>
-             <div className="card w-96 bg-base-100 shadow-xl">
-            <figure className="px-10 pt-10">
-                <img src={foodImage} alt="Shoes" className="rounded-xl" />
+             <div className="card w-96 bg-amber-200 shadow-xl">
+            <figure className="px-2 pt-2">
+                <img src={foodImage} alt="Shoes" className="rounded-xl h-[240px] w-full" />
             </figure>
             <div className="card-body">
                 <h2 className="card-title">{foodName}</h2>
-                <h2>{foodQuantity} <span>{expiredDateTime}</span></h2>
-                <p>{pickupLocation}</p>
-                <p>{additionalNotes}</p>
-               <div className="flex justify-between"> <p className="text-xl text-orange-500">{donatorName}</p>
-               <img src="" alt="" /></div>
-                <div className="card-actions">
+                <h2 className="font-bold">Food Quantity: {foodQuantity} <br /> <span>Expire date:{expiredDateTime}</span></h2>
+                <p>Location: {pickupLocation}</p>
+                <p>Notes: {additionalNotes}</p>
+                <p className="items-center justify-center flex text-amber-900 border-b-2">DONATED BY</p>
+               <div className="flex justify-between"> <p className="text-3xl mt-5 text-orange-500">{donatorName}</p>
+               <div className="avatar">
+  <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+    <img src={pic} />
+  </div>
+</div></div>
+                <div className="card-actions items-center">
                     <Link to={`/checkout/${_id}`}>
-                        <button className="btn bg-amber-400">View Details <ArrowRightIcon></ArrowRightIcon></button>
+                        <button className="btn bg-amber-400 w-full ">View Details <ArrowRightIcon></ArrowRightIcon></button>
                     </Link>
                 </div>
             </div>
--- src/Layout/Pages/Feature/Features.jsx
+++ src/Layout/Pages/Feature/Features.jsx
@@ -12,12 +12,12 @@ const Features = () => {
     return (
         <div>
              <div className="text-center">
-                <h3 className="text-2xl font-bold text-orange-600">Our Services</h3>
-                <h2 className="text-5xl">Feature FOOD</h2>
-                <p>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable. </p>
+                <h3 className="text-2xl font-bold text-amber-600">Food for donation</h3>
+                <h2 className="text-5xl text-amber-400">Feature FOOD</h2>
+                <p className="text-amber-600 text-xl mb-2">Discover a world of culinary generosity on our food sharing platform. </p>
         
             </div>
-           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
+           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ml-10">
            {
                 feature.map(feature=><FeatureCard key={feature._id} feature={feature}></FeatureCard>)
             }
